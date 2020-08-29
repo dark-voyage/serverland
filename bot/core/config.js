70c9971eea4ca8689331cede44d21f550dffb035
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-29 05:08:43 +0500

    Update config.js

--- bot/core/config.js
+++ bot/core/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: process.env.BOT_TOKEN,
-    owner: process.env.BOT_OWNER | 756870298,
+    owner: process.env.BOT_OWNER,
     domain: process.env.BOT_URL,
     port: process.env.BOT_PORT,
     env: process.env.HOST
