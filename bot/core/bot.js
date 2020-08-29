fd15df6b046bbbdf4e2ba297bdfa2a716a34714c
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-29 06:40:11 +0500

    Update bot.js

--- bot/core/bot.js
+++ bot/core/bot.js
@@ -28,7 +28,7 @@ const launch = async () => {
             //     })
         // Polling method
             await bot.telegram.deleteWebhook()
-            await bot.startPolling(100)
+            await bot.startPolling(1000)
     } else if (process.env.HOST === "local") {
         await bot.launch()
             .then(async () => {
