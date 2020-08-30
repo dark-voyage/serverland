db7b15e6e64fa779fc400dc5f9932f4e269e8faa
Sokhibjon Orzikulov <genemators@gmail.com>
2020-08-31 00:34:47 +0500

    Update database.config.js

--- apps/config/database.config.js
+++ apps/config/database.config.js
@@ -10,5 +10,6 @@ const env = require("./env.config");
 module.exports = () => {
   if (env.HOST === "heroku") return env.DB_HOST;
   else if (env.HOST === "test") return null;
-  else return "mongodb://localhost:27017/serverland";
+  else if (env.HOST === "local") return "mongodb://localhost:27017/serverland";
+  else return null;
 };
