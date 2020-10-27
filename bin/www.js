c649b7c4486ee25ed3e222064135a549a13d708c
Geno Ferollo <genemators@gmail.com>
2020-10-28 03:58:00 +0500

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -17,12 +17,12 @@ require("../apps/core/server")
     await console.log(`Error occurred while executing: ${error}`.red.bold);
   });
 
-// Launching up telegram bot with Long Polling session
-require("../bot/core/bot")
-  .launch()
-  .then(async () => {
-    await console.log("Bot has been started".green.bold);
-  })
-  .catch(async (error) => {
-    await console.log(`Error occurred while executing: ${error}`.red.bold);
-  });
+// // Launching up telegram bot with Long Polling session
+// require("../bot/core/bot")
+//   .launch()
+//   .then(async () => {
+//     await console.log("Bot has been started".green.bold);
+//   })
+//   .catch(async (error) => {
+//     await console.log(`Error occurred while executing: ${error}`.red.bold);
+//   });
